4128a9bcbc529432f66922f2a355cc27336e8a71
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-03-19 11:31:21 +0100

    more data + design

--- build.js
+++ build.js
@@ -1,20 +1,20 @@
 const fs = require("fs")
 const path = require("path")
 
-fs.readdir(path.join(__dirname, "bots"), (err, files) => {
-	if (err) throw err
+fs.readdir(path.join(__dirname, "bots"), (err1, files) => {
+	if (err1) throw err1
 	console.log("All bots: ", files)
 
 	const list = []
 	files.forEach(file => {
-		fs.readFile(path.join(__dirname, "bots", file), (err, data) => {
-			if (err) throw err
+		fs.readFile(path.join(__dirname, "bots", file), (err2, data) => {
+			if (err2) throw err2
 			const parsed = JSON.parse(data)
 			list.push({id: parsed.id, name: parsed.name, discrim: parsed.discrim, avatar: parsed.avatar})
 
 			if (list.length == files.length) {
-				fs.writeFile(path.join(__dirname, "list.json"), JSON.stringify(list), err => {
-					if (err) throw err
+				fs.writeFile(path.join(__dirname, "list.json"), JSON.stringify(list), err3 => {
+					if (err3) throw err3
 					console.log("List of bots created")
 				})
 			}
