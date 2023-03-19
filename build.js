2c6099f5b2c74e7c7e89f37e75185cfe3b3f9b0d
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-03-19 11:07:19 +0100

    more content

--- build.js
+++ build.js
@@ -10,7 +10,7 @@ fs.readdir(path.join(__dirname, "bots"), (err, files) => {
 		fs.readFile(path.join(__dirname, "bots", file), (err, data) => {
 			if (err) throw err
 			const parsed = JSON.parse(data)
-			list.push({id: parsed.id, name: parsed.name})
+			list.push({id: parsed.id, name: parsed.name, discrim: parsed.discrim, avatar: parsed.avatar})
 
 			if (list.length == files.length) {
 				fs.writeFile(path.join(__dirname, "dist", "list.json"), JSON.stringify(list), err => {
