10c1bab16c3c3a57e5469c4f607d7d5cc2b6854d
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-03-19 10:42:01 +0100

    test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CompareBots
- A 100% needed project to compare the features of multipurpose bots
+A 100% needed project to compare the features of multipurpose Discord bots
+*sarcasm*
---
+++ build.js
@@ -0,0 +1,23 @@
+const fs = require("fs")
+const path = require("path")
+
+fs.readdir(path.join(__dirname, "bots"), (err, files) => {
+	if (err) throw err
+	console.log("Bot files: ", files)
+
+	const list = []
+	files.forEach(file => {
+		fs.readFile(path.join(__dirname, "bots", file), (err, data) => {
+			if (err) throw err
+			const parsed = JSON.parse(data)
+			list.push({id: parsed.id, name: parsed.name})
+
+			if (list.length == files.length) {
+				fs.writeFile(path.join(__dirname, "dist", "list.json"), JSON.stringify(list), err => {
+					if (err) throw err
+					console.log("List built successfully")
+				})
+			}
+		})
+	})
+})
